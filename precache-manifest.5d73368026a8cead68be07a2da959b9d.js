b0cdd27d8214a1b9993c92bb2284a258f413dc06
Shivanshp007 <shivashp007@gmail.com>
2025-04-29 16:18:32 +0530

    Updates

---
+++ precache-manifest.5d73368026a8cead68be07a2da959b9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914dd961ef9f6fb7b92d02fa6721d0eb",
+    "url": "/Text-Magic/index.html"
+  },
+  {
+    "revision": "8bc8e1d385838218a305",
+    "url": "/Text-Magic/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1ee19754989c0c8589e0",
+    "url": "/Text-Magic/static/js/2.0d8d0dc0.chunk.js"
+  },
+  {
+    "revision": "2ad33e9abe21e6769bbc",
+    "url": "/Text-Magic/static/js/3.bd282c0b.chunk.js"
+  },
+  {
+    "revision": "8bc8e1d385838218a305",
+    "url": "/Text-Magic/static/js/main.c77d9b76.chunk.js"
+  },
+  {
+    "revision": "80b35d3ec1288807205d",
+    "url": "/Text-Magic/static/js/runtime~main.67275dbd.js"
+  }
+]);
